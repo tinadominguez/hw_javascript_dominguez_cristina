124d6c95d8d05d6a956623c5d7ac89a16aebda9a
Tina Dominguez <tina@tinadominguez.com>
2015-09-25 19:16:09 -0400

    functions
    added functions to JS

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// function declaration
+
+var person ["Tina Dominguez"]
+
+var message ["hi"]
+
+function howdy(person, message) {
+        console.log(person + ' says' + message);
+    }
+
+//function expression
+
+var numba ["string"]
+
+if (numba > 7) {
+    console.log('What a short little word!');
+} 
+
+else if (numba = 7 {
+    console.log('7, what a perfect choice!');
+}
+else {
+    console.log('I’m sorry, but that’s too many to count.');
+}
+
+//function inception
+
+function inception (callback, value) {
+    callback(value);
+}
