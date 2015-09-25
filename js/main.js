373b818a60382eab1d5b8501d6cd9d19f0cba83e
Tina Dominguez <tina@tinadominguez.com>
2015-09-25 19:04:43 -0400

    presidents
    the last five presidents

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var Obama = {
+    firstName: 'Barack',
+    lastName: 'Obama',
+    termLength: 'two terms',
+    party: 'democrat',
+    yearsofPresidency: 'seven',
+};
+
+var Bush = {
+    firstName: 'George W.',
+    lastName: 'Bush',
+    termLength: 'two terms',
+    party: 'republican',
+    yearsofPresidency: 'eight',
+};
+
+var Clinton = {
+    firstName: 'Bill',
+   	lastName: 'Clinton',
+    termLength: 'two terms',
+    party: 'democrat',
+    yearsofPresidency: 'eight',
+};
+
+var Bush Sr = {
+    firstName: 'George H.W',
+    lastName: 'Bush',
+    termLength: 'one terms',
+    party: 'republican',
+    yearsofPresidency: 'four',
+};
+
+var Reagan = {
+    firstName: 'Ronald',
+    lastName: 'Reagan',
+    termLength: 'two terms',
+    party: 'republican',
+    yearsofPresidency: 'eight',
+};
+
+var presidents = [
+    'Obama',
+    'Bush',
+    'Clinton',
+    'Bush Sr',
+    'Reagan',
+];
+    
+
+function presidents(2) [
+        console.log('firstName' + 'lastName');
+]
